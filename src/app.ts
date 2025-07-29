214c67c4863505f9793b2999b8cb3b71a64556ed
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-07-29 16:22:43 +0600

    server setup done

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import { router } from "./app/routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Welcome to Ride Booking System Backend!"
+    })
+})
+
+export default app;
